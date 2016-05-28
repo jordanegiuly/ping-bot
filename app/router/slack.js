f371b54b02bb05dd82b3347a9ffb40a6481a6f0f
Jordane Giuly <jordane.giuly@gmail.com>
2016-05-28 14:01:15 +0200

    try to response to slack

--- app/router/slack.js
+++ app/router/slack.js
@@ -5,15 +5,20 @@ module.exports = () => {
     const slackRouter = require('express').Router();
 
     slackRouter.post('/ping', (req, res) => {
-        const slack_response = req.body;
-        console.log(slack_response.token);
-        request.post(slack_response.response_url,
-            {form: {text: 'OK'}}, (err, resp, body) => {
-                console.log('err', err);
-                console.log('resp', resp.statusCode);
-                console.log('body', body);
-                res.send('OK');
-        });
+        const body = req.body;
+        if (body.token && body.token === process.env.SLACK_TOKEN) {
+            res.status(200).send(body.text + ' not available.');
+        }
+        else {
+            res.status(401).send('Unauthorized');
+        }
+        // request.post(body.response_url,
+        //     {form: {text: 'OK'}}, (err, resp, body) => {
+        //         console.log('err', err);
+        //         console.log('resp', resp.statusCode);
+        //         console.log('body', body);
+        //         res.send('OK');
+        // });
     });
 
     return slackRouter;
