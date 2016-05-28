acf1c2283d2dd6f22a3b55eab34e3ee28941b0b2
Jordane Giuly <jordane.giuly@gmail.com>
2016-05-28 13:46:27 +0200

    try to response to slack

--- app/router/slack.js
+++ app/router/slack.js
@@ -1,11 +1,19 @@
 'use strict';
+const request = require('request');
 
 module.exports = () => {
     const slackRouter = require('express').Router();
 
     slackRouter.post('/ping', (req, res) => {
-        console.log('req.body', req.body);
-        res.send(req.body);
+        const slack_response = req.body;
+        console.log(slack_response.token);
+        request.post(slack_response.response_url,
+            {form: 'OK'}, (err, resp, body) => {
+                console.log('err', err);
+                console.log('resp', resp.statusCode);
+                console.log('body', body);
+                res.send('OK');
+        });
     });
 
     return slackRouter;
