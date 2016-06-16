480d8447fa8b58461f51cea502534aee76d6508d
Jordane Giuly <jordane.giuly@gmail.com>
2016-06-16 18:03:38 +0100

    print body

--- app/router/slack.js
+++ app/router/slack.js
@@ -1,12 +1,27 @@
 'use strict';
 
+const request = require('request');
+
 module.exports = () => {
     const slackRouter = require('express').Router();
 
+    slackRouter.get('/', (req, res) => {
+        const code = req.query.code;
+        exchangeCodeForToken(code, (err, token) => {
+            if (err) {
+                res.status(500).send('KO');
+            }
+            res.status(200).send('OK');
+        });
+    });
+
     slackRouter.post('/ping', (req, res) => {
         const body = req.body;
-        if (body.token && body.token === process.env.SLACK_TOKEN) {
+        console.log('body', body);
+        if (body.token === process.env.SLACK_TOKEN) {
+            // todo set content-type to application/json
             res.status(200).json({
+                response_type: 'in_channel', // for virality
                 text: body.text + ' has not authorized /ping yet.',
                 attachments: [
                     {
@@ -22,3 +37,21 @@ module.exports = () => {
 
     return slackRouter;
 };
+
+function exchangeCodeForToken(code, cb) {
+    request.post('https://slack.com/api/oauth.access', {
+        form: {
+            code,
+            client_id: '20872315939.46506921396',
+            client_secret: '88d5e04b440846506a5633dc952d0e17'
+        }
+    }, (err, res, body) => {
+        console.log('body', body);
+        if (body.ok) {
+            return cb(null, body);
+        } else {
+            console.log('err', err);
+            return cb(err, null);
+        }
+    });
+}
